d20d057e34222c257219884f4412263c7b1021cf
Hector Mauricio Forero Correa <mauriciocorrea.1015@gmail.com>
2022-09-10 12:09:36 -0500

    instalador automatico de librerias

---
+++ InstaladorDeLibrerias.bat
@@ -0,0 +1,15 @@
+@echo off
+start https://python.org/downloads/
+pip install SpeechRecognition
+pip install pyttsx3
+pip install pyAudio
+@REM  (en la carpeta donde esta el archivo que descargamos de PyAudio)
+pip install pywhatkit
+pip install pygame
+pip install keyboard
+pip install numpy
+pip install opencv-contrib-python
+pip install wikipedia
+pip install cmake
+pip install dlib
+pip install face_recognition
