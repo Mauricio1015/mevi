372c6ca92eba3a74ee6944d51503dcd6704e1efb
Mauricio1015 <mauriciocorrea.1015@gmail.com>
2022-09-11 13:51:59 -0500

    nuevo intalador automatico para linux

---
+++ InstaladorDeLibrerias.bash
@@ -0,0 +1,14 @@
+
+sudo apt-get install python3-pyaudio
+pip install SpeechRecognition
+pip install pyttsx3
+pip install pyAudio
+pip install pywhatkit
+pip install pygame
+pip install keyboard
+pip install numpy
+pip install opencv-contrib-python
+pip install wikipedia
+pip install cmake
+pip install dlib
+pip install face_recognition
